cae987623a193e4a86a6ae6e964d4cc00524c999
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2019-10-22 15:58:02 +0200

    Update template_code.js

--- template_code.js
+++ template_code.js
@@ -9,4 +9,4 @@ setInWindow('satismeter.l', getTimestamp(), false);
 const url = 'https://app.satismeter.com/satismeter.js';
 injectScript(url, data.gtmOnSuccess, data.gtmOnFailure, url);
 
-satismeter({writeKey: 'oo6DEzaTqH3vaIhy'});
+satismeter({writeKey: data.writeKey});
